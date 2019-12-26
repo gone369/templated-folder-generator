5a049de6e49a286eba76e81d143df4887962c709
尘凨 <xun.xc@antfin.com>
2019-12-26 12:36:46 +0800

    add testEnv

---
+++ testEnv/src/test2/test2.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './test2.css'
+
+export default Test2 extends Component{
+  static defaultProps = {
+  }
+  state = {
+  }
+  /*
+  componentWillMount(){
+  }
+  componentDidMount(){
+  }
+  shouldComponentUpdate(nextProps,nextState){
+    return true;
+  }
+  componentWillReceiveProps(nextProps,nextState){
+    return true;
+  }
+  componentWillUpdate(nextProps,nextState){
+  }
+  componentDidUpdate(prevProps,prevState){
+  }
+  componentWillUnmount(prevProps,prevState){
+  }
+  */
+  render(){
+    return (
+      <div className="test2">
+        test2
+      </div>
+    )
+  }
+}
